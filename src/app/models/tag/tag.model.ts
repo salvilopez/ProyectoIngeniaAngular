55c816715fea0f2c9f17f39b64ce990d00aa6147
Salvador Sierra <salvilopezronda@gmail.com>
2021-04-19 19:34:53 +0200

    add campo tags

--- src/app/models/tag/tag.model.ts
+++ src/app/models/tag/tag.model.ts
@@ -5,7 +5,12 @@ export class Tag implements Itag {
   id: number = 0;
   nombre: string;
   expertList: Expert[] = [];
-  constructor(nombre: string) {
+  created_at: Date;
+  updated_at: Date;
+  constructor(nombre: string,   created_at: Date,
+    updated_at: Date,) {
     this.nombre = nombre;
+    this.created_at = created_at;
+    this.updated_at = updated_at;
   }
 }
