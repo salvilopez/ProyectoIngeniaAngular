6569dae6ad731bbafef0a842c16017bc41fd70d4
salvi <salvilopezronda@gmail.com>
2021-05-08 19:56:52 +0200

    pruebas

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -9,7 +9,7 @@ import { User } from 'src/app/models/user/user.model';
   providedIn: 'root'
 })
 export class AuthService {
-  urlbase:string='https://proyectofinal-ingenia.herokuapp.com'
+  urlbase:string='http://proyectofinal-ingenia.herokuapp.com'
 //urlbase:string='http://localhost:8081'
   private isLoggedIn: boolean = false;
   constructor(private http: HttpClient, private sanitizer: DomSanitizer) {}
